48542f49c7d0e738834394ee1be840322b9eacb4
qm <baosheng.zhang@questmobile.com.cn>
2020-06-01 19:36:20 +0800

    实现获取用户发布视频列表脚本

--- bilibili/bilibili/items.py
+++ bilibili/bilibili/items.py
@@ -16,3 +16,19 @@ class UserInfo(scrapy.Item):
     face = scrapy.Field()
     follower = scrapy.Field()
     following = scrapy.Field()
+
+
+class VideosItem(scrapy.Item):
+    video_aid = scrapy.Field()  # 评论所需的oid的值
+    video_android_url = scrapy.Field()  # 评论url
+    video_bid = scrapy.Field()  # 视频播放页视频id
+    video_web_url = scrapy.Field()  # 视频播放url
+    video_author = scrapy.Field()  # 视频作者
+    author_id = scrapy.Field()  # 作者id
+    comment_count = scrapy.Field()  # 评论总数
+    video_id = scrapy.Field() # 视频
+    play_count = scrapy.Field()
+    description = scrapy.Field()
+    title = scrapy.Field()
+    created = scrapy.Field()
+    video_length = scrapy.Field()
