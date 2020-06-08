d44f06f6b39cf16c4c043ce38baeacdd9a47169f
qm <baosheng.zhang@questmobile.com.cn>
2020-06-08 19:33:00 +0800

    update

--- docker_bilibili/bilibili/middlewares.py
+++ docker_bilibili/bilibili/middlewares.py
@@ -53,13 +53,13 @@ class RandomUserAgentMiddleware(object):
         request.headers['User-Agent'] = user_agent
 
 
-def random_proxy():
+def redom_proxy():
     """获取一个随机代理"""
-    response = requests.get(proxy_url)
-    response = response.text
-    result = json.loads(response)
+    url = proxy_url
+    result = json.loads(requests.get(url).text)
     proxy_list = result.get('data')
     proxy_count = len(proxy_list)
+
     num = random.randint(0, proxy_count)
     ip = proxy_list[num].get('IP')
     port = proxy_list[num].get('Port')
@@ -75,16 +75,12 @@ class ProxiesMiddleware:
                       IOError, TunnelError)
 
     def __init__(self):
-        try:
-            self.proxy = random_proxy()  # 随机获取一个代理方法
-        except:
-            self.proxy = random_proxy()  # 随机获取一个代理方法
-
+        self.proxy = redom_proxy()  # 随机获取一个代理方法
         self.count = 0
 
     def process_request(self, request, spider):
         if self.count % 500 == 0:
-            self.proxy = random_proxy()
+            self.proxy = redom_proxy()
         self.count += 1
         spider.logger.info("[proxy]   {}".format(self.proxy))
         request.meta["proxy"] = self.proxy
@@ -92,12 +88,11 @@ class ProxiesMiddleware:
     def process_response(self, request, response, spider):
         # 因为遇到过那种返回状态码是200但是是一个被反扒的界面，界面固定都是小于3000字符
         # if len(response.text) < 3000 or response.status in [403, 400, 405, 301, 302, 418]:
-        if response.status in [403, 400, 405, 301, 302, 402, 418]:
+        if response.status in [403, 400, 405, 301, 302, 418]:
             spider.logger.info("[此代理报错]   {}".format(self.proxy))
-            new_proxy = random_proxy()
+            new_proxy = redom_proxy()
             self.proxy = new_proxy
             spider.logger.info("[更的的新代理为]   {}".format(self.proxy))
-            # break
             new_request = request.copy()
             new_request_l = new_request.replace(url=request.url)
             return new_request_l
@@ -109,7 +104,7 @@ class ProxiesMiddleware:
             # 在日志中打印异常类型
             spider.logger.info("[Got exception]   {}".format(exception))
             spider.logger.info("[需要更换代理重试]   {}".format(self.proxy))
-            new_proxy = random_proxy()
+            new_proxy = redom_proxy()
             self.proxy = new_proxy
             spider.logger.info("[更换后的代理为]   {}".format(self.proxy))
             new_request = request.copy()
