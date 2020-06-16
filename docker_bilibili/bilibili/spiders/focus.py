4443d324ab4ab54364dd912833afded36df1e082
qm <baosheng.zhang@questmobile.com.cn>
2020-06-16 17:11:18 +0800

    修改docker版本哔哩哔哩用户关注信息采集程序

--- docker_bilibili/bilibili/spiders/focus.py
+++ docker_bilibili/bilibili/spiders/focus.py
@@ -1,53 +1,108 @@
 import json
 from scrapy.http import Request
 from scrapy_redis.spiders import RedisSpider
-
+import requests
 from items import UserFocus
+import random
 
 
 class FocusSpider(RedisSpider):
     name = 'focus'
     allowed_domains = ['api.bilibili.com']
     redis_key = "bili_focus:start_urls"
-    # focus-follow 的接口
+    second_page_url = 'https://api.bilibili.com/x/relation/followings?vmid={}&pn=2&ps=50&order=desc&jsonp=jsonp'
+    # 获取用户关注数，粉丝数 的接口
     follow_info_url = 'https://api.bilibili.com/x/relation/stat?vmid={}&jsonp=jsonp'
+
+    # 获取用户关注数，粉丝数 的接口
+
+    @staticmethod
+    def get_proxy():
+        url = 'http://ip.ipjldl.com/index.php/api/entry?method=proxyServer.hdtiqu_api_url&packid=0&fa=0&groupid=0&fetch_key=&time=100&qty=5&port=1&format=json&ss=5&css=&dt=0&pro=&city=&usertype=4'
+        ret = requests.get(url).text
+        result = json.loads(ret)
+        proxy_item = random.choice(result.get('data'))
+        IP = proxy_item.get('IP')
+        Port = proxy_item.get('Port')
+        proxy = {
+            'https': "https://{}:{}".format(IP, Port),
+            'http': "https://{}:{}".format(IP, Port)
+        }
+        return proxy
 
     def parse(self, response):
         ret_dict = json.loads(response.text)
         status_code = ret_dict.get('code')
-        focus_info = UserFocus()
         if not status_code:
             if 'data' in ret_dict.keys():
                 info_dict = ret_dict.get('data')
-                # 总共关注了多少人
-                if 'pn=1' in response.url:
-                    total = info_dict.get('total')
-                    page_count = total // 20 + 2
-                    page_count = 6 if page_count > 6 else page_count
-                    for page in range(2, page_count):
-                        page_url = response.url.replace('pn=1', 'pn={}'.format(page))
-                        yield Request(page_url, self.parse, dont_filter=True, meta=response.meta)
+                total = info_dict.get('total')
 
                 focus_list = info_dict.get('list')
                 user_id = response.url.strip('https://api.bilibili.com/x/relation/followings?vmid=').split('&')[0]
-                for focus in focus_list:
-                    focus_info['user_id'] = user_id
-                    focus_info['focus_id'] = focus.get('mid')
-                    focus_info['focus_name'] = focus.get('uname')
-                    focus_info['focus_face'] = focus.get('face')
-                    focus_info['introduction'] = focus.get('sign')
-                    response.meta['item'] = focus_info
-                    yield Request(url=self.follow_info_url.format(focus_info.get('focus_id')),
-                                  meta=response.meta,
-                                  callback=self.get_follow_info)
+                item = dict()
+                item['user_id'] = user_id
+                item['focus_info'] = list()
+                for focus_item in focus_list:
+                    focus = dict()
+                    focus['focus_id'] = focus_item.get('mid')
+                    focus['focus_name'] = focus_item.get('uname')
+                    focus['focus_face'] = focus_item.get('face')
+                    focus['introduction'] = focus_item.get('sign')
+                    try:
+                        focus = self.get_follow_info(focus)
+                    except:
+                        self.proxies = self.get_proxy()
+                        focus = self.get_follow_info(focus)
+                    item['focus_info'].append(focus)
 
-    def get_follow_info(self, response):
-        focus_info = response.meta['item']
+                if total <= 50:
+                    # 直接一次获取用户的数据
+                    yield item
+
+                else:
+                    # yield 直接放第二页的url
+
+                    response.meta['item'] = item
+                    yield Request(url=self.second_page_url.format(item.get('user_id')),
+                                  meta=response.meta,
+                                  callback=self.parse_second_page)
+
+    def parse_second_page(self, response):
+        """
+        处理第二页,将 focus item 放到列表中在循环获取
+        :param response:
+        :return:
+        """
+        item = response.meta['item']
         ret_dict = json.loads(response.text)
         status_code = ret_dict.get('code')
         if not status_code:
             if 'data' in ret_dict.keys():
+                info_dict = ret_dict.get('data')
+                focus_list = info_dict.get('list')
+                for focus_item in focus_list:
+                    focus = dict()
+                    focus['focus_id'] = focus_item.get('mid')
+                    focus['focus_name'] = focus_item.get('uname')
+                    focus['focus_face'] = focus_item.get('face')
+                    focus['introduction'] = focus_item.get('sign')
+                    try:
+                        focus = self.get_follow_info(focus)
+                    except:
+                        self.proxies = self.get_proxy()
+                        focus = self.get_follow_info(focus)
+                    item['focus_info'].append(focus)
+                yield item
+
+    def get_follow_info(self, focus_item, proxies=None):
+        focus_info_url = self.follow_info_url.format(focus_item.get('focus_id'))
+        ret = requests.get(focus_info_url, proxies=proxies).text
+        ret_dict = json.loads(ret)
+        status_code = ret_dict.get('code')
+        if not status_code:
+            if 'data' in ret_dict.keys():
                 info_dict = ret_dict['data']
-                focus_info['focus_fans'] = info_dict.get('following')
-                focus_info['focus_focus'] = info_dict.get('follower')
-                yield focus_info
+                focus_item['focus_fans'] = info_dict.get('following')
+                focus_item['focus_focus'] = info_dict.get('follower')
+                return focus_item
