edc81feddb1f48476a2a0463b512c5e2fa35b167
qm <baosheng.zhang@questmobile.com.cn>
2020-06-17 15:31:31 +0800

    mongo--添加更新mongo中数据的方法

--- bilibili/bilibili/pipelines.py
+++ bilibili/bilibili/pipelines.py
@@ -14,8 +14,30 @@ class BiLiBiLiPipeline(object):
         if spider.name == 'user':
             self.insert_item(self.User, item)
         elif spider.name == 'focus':
-            self.insert_item(self.Focus, item)
+            key = {"user_id": item.get('user_id'), 'total': item.get('total')}
+            add_state = {'$addToSet': {'focus_list': {"focus_id": item.get('focus_id'),
+                                                      'focus_name': item.get('focus_name'),
+                                                      'focus_face': item.get('focus_face'),
+                                                      'introduction': item.get('introduction')
+                                                      }}}
+            self.update_info(key, add_state, self.Focus)
         return item
+
+    @staticmethod
+    def update_info(key, add_state, collection):
+        """
+
+        :param key: 此处表示 user_id
+        :param add_state: {'$addToSet': {'list': {"focus": 5, 'name': '小明', 'age': 66}}}
+        :param collection: 集合对象
+        :return:
+        """
+        try:
+            # 不存在时插入数据
+            collection.update_one(key, add_state, upsert=True)
+            print("更新成功")
+        except Exception as e:
+            print("更新失败:{}".format(e))
 
     @staticmethod
     def insert_item(collection, item):
