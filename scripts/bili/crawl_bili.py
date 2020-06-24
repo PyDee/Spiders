868ccea4f82e9ccc75d3a857d055f6a9a730a260
qm <baosheng.zhang@questmobile.com.cn>
2020-06-24 19:56:46 +0800

    script-测试bili获取用户关注脚本

--- scripts/bili/crawl_bili.py
+++ scripts/bili/crawl_bili.py
@@ -1,119 +1,123 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 18 17:34:20 2018
-@author: peter
-"""
-import sqlite3
+import redis
 import requests
-import time
+import json
+import pymongo
 
-startid = 35365573
-user = [startid]
+user_id_list = [
+    '100004470',
+    '100004989',
+    '100012099',
+    '100012471',
+    '100015726',
+    '100017751',
+    '100018921',
+    '10002030',
+    '100020355',
+    '10002255',
+    '100023481',
+    '10002549',
+    '100027426',
+    '100027543',
+    '100030195',
+    '10003542',
+    '10003698',
+    '100039840',
+    '10006776',
+    '10007583',
+    '10008087',
+    '100098616',
+    '10010784',
+    '100110226',
+    '100112359',
+    '10011954',
+    '100143736',
+    '10016637',
+    '100168060',
+    '100182379',
+    '100187317',
+    '10018800',
+    '100190635',
+    '100196947',
+    '10019715',
+    '100200898',
+    '10020234',
+    '100210348',
+    '100214869',
+    '100214938',
+    '100216915',
+    '10022142',
+    '100229167',
+    '100236136',
+    '10024569',
+    '10024707',
+    '100248892',
+    '100254985',
+    '10026546',
+    '100270528',
+]
+relation_url = 'https://api.bilibili.com/x/relation/followings?vmid={}&pn=1&ps=50&order=desc&jsonp=jsonp'
+
+r = redis.Redis(host="127.0.0.1")
 
 
-def create():
-    # 创建数据库
-    global conn
-    conn = sqlite3.connect('data.db')
-    conn.execute("""
-                create table if not exists user(
-                id INTEGER PRIMARY KEY ,
-                mid int DEFAULT NULL,
-                name varchar DEFAULT NULL,
-                sign varchar DEFAULT NULL)""")
-    conn.execute("""
-                create table if not exists relation(
-                id INTEGER PRIMARY KEY ,
-                master int,
-                following int 
-                )""")
-    conn.commit()
+class BiLiBiLiPipeline(object):
+    def __init__(self):
+        client = pymongo.MongoClient('127.0.0.1', 27017)
+        self.db = client['bili']
+        # self.User = db["user"]
+        # self.Focus = db["focus"]
 
-
-def save(result=[], master=0):
-    # 将数据保存至本地
-    global conn, user
-    if result == [] or master == 0:
-        print("save error!")
-        return
-    command1 = "insert into user \
-             (mid,name,sign) values (?,?,?);"
-    command2 = "insert into relation\
-             (master,following)values(?,?)"
-    for row in result:
+    def insert_item(self, collection_name, item):
+        collection = self.db[collection_name]
         try:
-            temp = (master, row[0])
-            if row[0] not in user:
-                user.append(row[0])
-                conn.execute(command1, row)
-                conn.execute(command2, temp)
-            else:
-                conn.execute(command2, temp)
-        except Exception as e:
-            print(e)
-            print("insert error!")
-            conn.rollback()
-    conn.commit()
-    result = []
+            collection.insert(dict(item))
+        except:
+            pass
 
 
-def func(startid=0):
-    global user
-    if startid == 0:
-        return
-    i = 0
-    result = []
-    ref_url = "https://space.bilibili.com/" + str(startid) + "/#/fans/follow"
-    head = {
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-        'DNT': '1',
-        'Host': 'api.bilibili.com',
-        'Pragma': 'no-cache',
-        'Referer': ref_url,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36'
-    }
-    while 1:
-        i += 1
-        if i >= 6:
-            break
-        url = "https://api.bilibili.com/x/relation/followings?vmid=" + \
-              str(startid) + "&pn=" + str(i) + \
-              "&ps=20&order=desc&jsonp=jsonp&callback=__jp5"
-        try:
-            r = requests.get(url, headers=head, timeout=10).text
-            r2 = eval(r[6:-1].replace('null', 'None'))
-            list1 = r2['data']['list']
-            if list1 == []:
-                break
-            else:
-                for user1 in list1:
-                    result.append(
-                        [user1["mid"], user1["uname"], user1["sign"]])
-        except Exception as e:
-            print(e)
-    if result != []:
-        save(result, startid)
+def get_relation(user_id):
+    response = requests.get(relation_url.format(user_id))
+    ret_dict = json.loads(response.text)
+    status_code = ret_dict.get('code')
+    if not status_code:
+        if 'data' in ret_dict.keys():
+            info_dict = ret_dict.get('data')
+            total = info_dict.get('total')
+            focus_list = info_dict.get('list')
+            for focus_item in focus_list:
+                focus_info = dict()
+                focus_info['user_id'] = user_id
+                focus_info['total'] = total
+                focus_info['focus_id'] = focus_item.get('mid')
+                focus_info['focus_name'] = focus_item.get('uname')
+                focus_info['focus_face'] = focus_item.get('face')
+                focus_info['introduction'] = focus_item.get('sign')
+
+                print(focus_info)
+                focus_url = 'https://api.bilibili.com/x/relation/stat?vmid={}&jsonp=jsonp'
+                focus_url = focus_url.format(focus_info.get('focus_id'))
+                r.lpush('test_focus_list', focus_url)
 
 
-if __name__ == "__main__":
-    create()
-    cycle = 0
-    recordids = 0
-    time0 = time.time()
-    while 1:
-        cycle += 1
-        if recordids == 0:
-            users = [startid]
-        else:
-            users = user[recordids:len(user)]
-        for i in users:
-            recordids += 1
-            func(i)
-            time1 = time.time()
-            print("\r已爬取{0}个用户，正在第{1}层 ，总花费时间:{2:.2f}s".format(
-                recordids, cycle, time1 - time0), end="")
+def get_focus_info(url):
+    response = requests.get(url)
+    ret_dict = json.loads(response.text)
+    status_code = ret_dict.get('code')
+    focus_item = dict()
+    if not status_code:
+        if 'data' in ret_dict.keys():
+            info_dict = ret_dict['data']
+            focus_item['mid'] = info_dict.get('mid')
+            focus_item['focus_fans'] = info_dict.get('following')
+            focus_item['focus_focus'] = info_dict.get('follower')
+            print(focus_item)
+
+
+def main():
+    # for user_id in user_id_list:
+    #     get_relation(user_id)
+    get_focus_info('https://api.bilibili.com/x/relation/stat?vmid=125269176&jsonp=jsonp')
+
+
+if __name__ == '__main__':
+    main()
