b14d8c8630abfb9f1c405e00c5ed981e8eb3dae5
qm <baosheng.zhang@questmobile.com.cn>
2020-07-16 16:41:40 +0800

    临时需求-update

--- tempspiders/tempspiders/settings.py
+++ tempspiders/tempspiders/settings.py
@@ -21,12 +21,12 @@ NEWSPIDER_MODULE = 'tempspiders.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 5
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.02
+DOWNLOAD_DELAY = 0.01
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
