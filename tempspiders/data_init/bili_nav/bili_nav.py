6590a3e1560a506d42ebb078bf41bd757bd54bf9
qm <baosheng.zhang@questmobile.com.cn>
2020-07-28 10:53:13 +0800

    临时-bili-重新获取没有获取到的id面包屑，读取mongo中数据进行比较

--- tempspiders/data_init/bili_nav/bili_nav.py
+++ tempspiders/data_init/bili_nav/bili_nav.py
@@ -1,5 +1,23 @@
 import redis
 import pandas as pd
+import pymongo
+
+
+class MongoDB:
+
+    def __init__(self):
+        self.m = pymongo.MongoClient("mongodb://localhost:27017/")
+
+    def read_uid(self):
+        result_list = list()
+        db = self.m["temp"]
+        c = db.navBili
+        result = c.find({}, {"mid": 1})
+        data = pd.DataFrame(list(result))
+        df_li = data.values.tolist()
+        for i in df_li:
+            result_list.append(i[1])
+        return result_list
 
 
 class RedisDB:
@@ -19,17 +37,25 @@ class RedisDB:
         result = self.r.lpop(key)
         return result
 
-    def run(self):
+    def run(self, result):
         df = pd.read_excel('./bili_nav.xlsx')
         df_li = df.values.tolist()
         count = 1000
+        # print(result)
         for s_li in df_li:
-            self.r.lpush('bili_nav', s_li[0])
-            count += 1
-            if count % 1000 == 0:
-                print('处理{}个数据'.format(count))
+            # print(s_li[0])
+            if int(s_li[0]) in result:
+                continue
+            else:
+                self.r.lpush('bili_nav', s_li[0])
+                count += 1
+                if count % 1000 == 0:
+                    print('处理{}个数据'.format(count))
 
 
 if __name__ == '__main__':
+    mdb = MongoDB()
+    result = mdb.read_uid()
+    #  如果是第一次处理，可设置result 为空列表
     rdb = RedisDB()
-    rdb.run()
+    rdb.run(result)
