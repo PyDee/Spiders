b5b05da4e1628a20f70e5a050c7329987f2eb259
qm <baosheng.zhang@questmobile.com.cn>
2020-08-11 16:07:20 +0800

    直播数据采集-YY直播-完善获取工会信息

--- jsparse/yy/getYyInfo.py
+++ jsparse/yy/getYyInfo.py
@@ -1,6 +1,7 @@
+"""YY 直播数据采集"""
 import json
 import requests
-from lxml import etree
+import re
 
 
 class YY:
@@ -18,26 +19,32 @@ class YY:
         # 用户信息 users-users-uid
         self.user_info = 'https://www.yy.com/api/liveInfoDetail/{}/{}/{}'
 
-    def get_live_info(self, users):
-        """请求直播页面， 解析工会 num 和用户 uid """
-        url = 'https://www.yy.com/{}/{}'.format(users, users)
-        ret = requests.get(url).text
-        doc = etree.HTML(ret)
-        uid = doc.xpath('//span[@class="w-liveplayer-head__follow-btn"]/@data-uid')
-        yyno = doc.xpath('//a[@class="anchor-name"]/@href')
-        self.get_labor_union(yyno=yyno)
-        return uid, yyno
-
-    def get_user_info(self, users, uid):
+    def get_user_info(self, sid, ssid, uid):
         """获取用户信息"""
-        ret = requests.get(self.user_info.format(users, users, uid)).text
-        print(ret)
+        ret = requests.get(self.user_info.format(sid, ssid, uid))
+        ret = ret.text
+        print("user_info:", ret)
 
-    def get_labor_union(self, yyno
-                        ):
+    @staticmethod
+    def get_live_page(live_url):
+        """获取直播页面内容，提取工会 yy_num"""
+        url = 'https://www.yy.com{}'.format(live_url)
+        ret = requests.get(url)
+        yy_no = re.search("yynum: (.*?),", ret.text).group(1).strip('"')
+        return yy_no
+
+    def get_labor_union(self, live_url):
+        #  todo 需要获取yyno
         """获取工会信息, 需要提取页面内容，即get_live_info"""
-        ret = requests.get(self.labor_union.format(yyno)).text
-        print(ret)
+        live_dict = dict()
+        yy_no = self.get_live_page(live_url)
+        ret = requests.get(self.labor_union.format(yy_no))
+        result = ret.json()
+        live_dict['lavor_name'] = result.get('data').get('name')
+        live_dict['lavor_rank'] = result.get('data').get('rank')
+        live_dict['lavor_type'] = result.get('data').get('type')
+        live_dict['lavor_yy_num'] = result.get('data').get('yy_num')
+        print("labor_info:", live_dict)
 
     @staticmethod
     def get_live_list(url):
@@ -53,20 +60,15 @@ class YY:
         for item in items:
             live_dict = dict()
             # todo 构造字典，保存入库
-            live_dict['id'] = item.get('id')
             live_dict['uid'] = item.get('uid')
-            live_dict['sid'] = item.get('sid')
-            live_dict['ssid'] = item.get('ssid')
-            live_dict['pid'] = item.get('pid')
             live_dict['name'] = item.get('name')
+            live_dict['desc'] = item.get('desc')
             live_dict['users'] = item.get('users')
             live_dict['biz'] = item.get('biz')
-            user_id, yyno = self.get_live_info(item.get('users'))
-            live_dict['user_id'] = user_id
-            live_dict['yyno'] = yyno
-            print(live_dict)
-            self.get_user_info(item.get('users'), user_id)
-            self.get_labor_union(yyno)
+            live_dict['liveUrl'] = item.get('liveUrl')
+            # self.get_user_info(item.get('sid'), item.get('ssid'), item.get('uid'))
+            self.get_labor_union(item.get('liveUrl'))
+            print("live_dict:", live_dict)
             break
 
     def run(self):
